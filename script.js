7f97ba290ba7533bce60095bde66a97bbdf5910f
pauloesmelos <email>
2022-12-06 20:53:22 -0300

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-/*1)PRIMEIROS PASSOS*/
-//se o git ainda não estiver configurado,faça isso:
+/*PARTE 1*/
 
-//1) git config --global user.name "seu nome"
-//2) git config --global user.email "email@gmail.com"
+//1)se o git ainda não estiver configurado,faça isso:
+//git config --global user.name "seu nome"
+//git config --global user.email "email@gmail.com"
 
 /*2)INICIANDO UM REPOSITORIO*/
-//1) git init
+//git init
 
 /*3)LISTANDO ALTERAÇÕES E ADICIONANDO ARQUIVOS PARA UM COMMIT*/
 //git status
@@ -15,3 +15,6 @@
 /*4)ADICIONAR UM REPOSITÓRIO GITHUB E FAZER PUSH PRO MESMO*/
 //git remote add origin url
 //git push -u origin master
+
+alert('finish');
+console.log('change');
